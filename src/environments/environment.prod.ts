41a4c2eec908a74795d9d0cfb97aaadea8a6e762
dkshaw39 <dkshaw39@gmail.com>
2020-08-09 17:37:55 +0530

    firebase prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,13 @@
 export const environment = {
   production: true,
-  url: 'https://api.app4school.co.in:8888'
+  url: 'https://api.app4school.co.in:8888',
+  firebase: {
+    apiKey: 'AIzaSyBoVZ6EEoxJQrriJsv-BuIpsr3AhNxnqSw',
+    authDomain: 'e-classroom-ceb7d.firebaseapp.com',
+    databaseURL: 'https://e-classroom-ceb7d.firebaseio.com',
+    projectId: 'e-classroom-ceb7d',
+    storageBucket: 'e-classroom-ceb7d.appspot.com',
+    messagingSenderId: '724930077216',
+    appId: '1:724930077216:web:bd797cdfc9ff87e7cad245'
+  }
 };
